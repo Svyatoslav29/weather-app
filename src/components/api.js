789b2ae3aee1a35bd955fc62bc69912cadf11411
Svyatoslav29 <svyatoslav29@gmail.com>
2022-12-30 01:28:23 +0200

    Weather app

---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "da735500f6msh0d2dcecd8d4d480p1c80ebjsna223c2b9c775",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GE0_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "04caafe2adc3d4c83d34ae941beb1778";
